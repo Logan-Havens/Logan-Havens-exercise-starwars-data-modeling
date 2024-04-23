672372da9732b3011fcb94cd15932ffe319594d0
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-04-23 00:46:10 +0000

    finshed i think

--- src/models.py
+++ src/models.py
@@ -1,32 +1,81 @@
+from __future__ import annotations
+
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from typing import List
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Table
+from sqlalchemy.orm import relationship, declarative_base, Mapped
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+user_planet_favorite = Table(
+    "UserPlanetFavorite",
+    Base.metadata,
+    Column("user_id", ForeignKey("User.id"), primary_key=True),
+    Column("planet_id", ForeignKey("Planet.id"), primary_key=True),
+)
 
-    def to_dict(self):
-        return {}
+
+user_starship_favorite = Table(
+    "UserStarshipFavorite",
+    Base.metadata,
+    Column("user_id", ForeignKey("User.id"), primary_key=True),
+    Column("starship_id", ForeignKey("Starship.id"), primary_key=True),
+)
+
+user_character_favorite = Table(
+    "UserCharacterFavorite",
+    Base.metadata,
+    Column("user_id", ForeignKey("User.id"), primary_key=True),
+    Column("character_id", ForeignKey("Character.id"), primary_key=True),
+)
+
+class User(Base):
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    favorites: Mapped[List[Planet]] = relationship("Planet", secondary=user_planet_favorite)
+    starship_favorites: Mapped[List[Starship]] = relationship("Starship", secondary=user_starship_favorite)
+    character_favorites: Mapped[List[Character]] = relationship("Character", secondary=user_character_favorite)
+
+class Planet(Base):
+    __tablename__ = "Planet"
+
+    id = Column(Integer, primary_key=True)
+    population = Column(Integer,nullable=False)
+    climate = Column(String, nullable=False)
+    surface_water_percentage = Column(String, nullable=False)
+    radius = Column(Float, nullable=False)
+    gravity = Column(Float, nullable=False)
+    favorites: Mapped[List[User]] = relationship("User", secondary=user_planet_favorite)
+
+class Starship(Base):
+    __tablename__ = "Starship"
+
+    id = Column(Integer, primary_key=True)
+    Class = Column(String ,nullable=False)
+    Crew_Members= Column(String ,nullable=False)
+    Passengers=Column(String ,nullable=False)
+    Length=Column(Integer ,nullable=False)
+    starship_favorites: Mapped[List[User]] = relationship("User", secondary=user_starship_favorite)
+
+class Character(Base):
+    __tablename__ = "Character"
+
+    id = Column(Integer, primary_key=True)
+    Height = Column(Integer ,nullable=False)
+    Mass= Column(Integer ,nullable=False)
+    Hair_Color=Column(String ,nullable=False)
+    Skin_Color=Column(String ,nullable=False)
+    character_favorites: Mapped[List[User]] = relationship("User", secondary=user_character_favorite)
+    
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
